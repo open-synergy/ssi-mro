85ab251ebd84ca84c4ecd0f13f6da4dc2f7f8f11
Andhitia Rama <andhitia.r@gmail.com>
2026-01-03 17:12:25 +0700

    [ADD] ssi_mro

--- ssi_mro/__manifest__.py
+++ ssi_mro/__manifest__.py
@@ -12,6 +12,12 @@
     "depends": [
         "ssi_master_data_mixin",
         "ssi_transaction_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_m2o_configurator_mixin",
+        "ssi_transaction_cancel_mixin",
         "ssi_stock",
     ],
     "data": [
